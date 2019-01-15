da481cd816ebc1680021f6f8397f81dbf6917096
ry <rnakada90@gmail.com>
2019-01-14 19:10:08 -0500

    1/14/2019 7:10pm

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,4 @@
+
 // Initialize firebase
 var config = {
   apiKey: "AIzaSyBlH5qBxr5Q3z2JbR60gAOdytnbAPzXKLE",
@@ -12,39 +13,44 @@ firebase.initializeApp(config);
 // Assign a reference to the database to a variable named "database"
 var database = firebase.database();
 
-
 // capture button click
-$(".submitButton").on("click", function(event){
+$(".submitButton").on("click", function (event) {
   event.preventDefault();
 
+  // if input fields are empty alert is signaled
+  if ($.trim($(".train-input").val()) === "" || $.trim($(".destination-input").val()) === "" || $.trim($(".firstTrain-input").val()) === "" || $.trim($(".frequency-input").val()) === "") {
+    alert("Please fill additional information!");
+    return false;
+  }
+
+  // grabbing the value out of the input
   train = $(".train-input").val().trim();
   destination = $(".destination-input").val().trim();
   firstTrain = $(".firstTrain-input").val().trim();
   frequency = $(".frequency-input").val().trim();
-
+  // creating a object to store in firebase
   database.ref().push({
-
     train: train,
     destination: destination,
     firstTrain: firstTrain,
     frequency: frequency,
     dateAdded: firebase.database.ServerValue.TIMESTAMP
-
   });
-
+  // clear values
   $(".train-input").val("");
   $(".destination-input").val("");
   $(".firstTrain-input").val("");
   $(".frequency-input").val("");
 
 });
+// firebase watcher + initial loader
+database.ref().on("child_added", function (snapshot) {
 
-database.ref().on("child_added", function(childSnapshot){
-
-  var train = childSnapshot.val().train;
-  var destination = childSnapshot.val().destination;
-  var firstTrain = childSnapshot.val().firstTrain;
-  var frequency = childSnapshot.val().frequency;
+  // 
+  var train = snapshot.val().train;
+  var destination = snapshot.val().destination;
+  var firstTrain = snapshot.val().firstTrain;
+  var frequency = snapshot.val().frequency;
   var firstTimeConverted = moment(firstTrain, "HH:mmA").subtract(1, "years");
   var currentTime = moment();
   var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
@@ -52,23 +58,24 @@ database.ref().on("child_added", function(childSnapshot){
   var minutesTillArrival = frequency - tRemainder;
   var nextTrain = moment().add(minutesTillArrival, "minutes");
 
-console.log("train name: " + train);
-console.log("destination: " + destination);
-console.log("first train time: " + firstTrain);
-console.log("frequency: " + frequency);
-console.log("first time converted: " + firstTimeConverted);
-console.log("current time: " + moment(currentTime).format("hh:mmA"));
-console.log("difference in time: " + diffTime);
-console.log("time remainder: " + tRemainder);
-console.log("minutes till arrival: " + minutesTillArrival);
-console.log("arrival: " + moment(nextTrain).format("hh:mmA"));
+  //console log 
+  console.log("train name: " + train);
+  console.log("destination: " + destination);
+  console.log("first train time: " + firstTrain);
+  console.log("frequency: " + frequency);
+  console.log("first time converted: " + firstTimeConverted);
+  console.log("current time: " + moment(currentTime).format("hh:mmA"));
+  console.log("difference in time: " + diffTime);
+  console.log("time remainder: " + tRemainder);
+  console.log("minutes till arrival: " + minutesTillArrival);
+  console.log("arrival: " + moment(nextTrain).format("hh:mmA"));
 
-$(".train-name").append("<li>" + childSnapshot.val().train + "</li>" + "<br>");
-$(".destination-name").append("<li>" + childSnapshot.val().destination + "</li>" + "<br>");
-$(".frequency-time").append("<li>" + childSnapshot.val().frequency + "</li>" + "<br>");
-$(".next-arrival").append("<li>" + moment(nextTrain).format("hh:mmA") + "</li>" + "<br>");
-$(".minutes-away").append("<li>" + minutesTillArrival + "</li>" + "<br>")
+  $(".train-name").append("<li>" + snapshot.val().train + "</li>" + "<br>");
+  $(".destination-name").append("<li>" + snapshot.val().destination + "</li>" + "<br>");
+  $(".frequency-time").append("<li>" + snapshot.val().frequency + "</li>" + "<br>");
+  $(".next-arrival").append("<li>" + moment(nextTrain).format("hh:mmA") + "</li>" + "<br>");
+  $(".minutes-away").append("<li>" + minutesTillArrival + "</li>" + "<br>")
 
-}, function(errorObject){
+}, function (errorObject) {
   console.log("Errors Handled: " + errorObject.code);
 });
